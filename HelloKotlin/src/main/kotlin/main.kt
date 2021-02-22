e96007a04b89c934982cf3e580cb8779e795d2de
rafaelalma <rafaelalma92@gmail.com>
2021-02-22 23:26:12 +0100

    Use a when expression

--- HelloKotlin/src/main/kotlin/main.kt
+++ HelloKotlin/src/main/kotlin/main.kt
@@ -6,11 +6,23 @@ fun main() {
 
 fun feedTheFish() {
     val randomDay = randomDay()
-    val food = "pellets"
+    val food = fishFood(randomDay)
     println("Today is $randomDay and the fish eat $food.")
 }
 
 fun randomDay(): String {
     val week = arrayOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
     return week[Random().nextInt(week.size)]
+}
+
+fun fishFood(day: String): String {
+    return when (day) {
+        "Monday" -> "flakes"
+        "Tuesday" -> "pellets"
+        "Wednesday" -> "redworms"
+        "Thursday" -> "granules"
+        "Friday" -> "mosquitoes"
+        "Saturday" -> "lettuce"
+        else -> "plankton"
+    }
 }
