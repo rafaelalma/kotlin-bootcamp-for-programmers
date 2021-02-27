82a3826e8222092615cc37b4a7885e50be96a1d1
rafaelalma <rafaelalma92@gmail.com>
2021-02-27 12:23:00 +0100

    Learn about Single Abstrack Methods

---
+++ FunctionalManipulation/src/main/kotlin/example/SAM.kt
@@ -0,0 +1,27 @@
+package example
+
+class KotlinRun {
+    companion object {
+        fun runNow(runnable: Runnable) {
+            runnable.run()
+        }
+    }
+}
+
+fun main() {
+    runExample()
+    runExample2()
+}
+
+fun runExample() {
+    val runnable = object: Runnable {
+        override fun run() {
+            println("I'm a Runnable")
+        }
+    }
+    KotlinRun.runNow(runnable)
+}
+
+fun runExample2() {
+    KotlinRun.runNow { println("Passing a lambda as a Runnable") }
+}
